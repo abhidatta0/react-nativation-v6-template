79dcc1ee26b1b282a72b35972812e01ad416b943
Abhirup <abhidatta146@gmail.com>
2022-03-19 21:17:04 +0530

    💄 added options of Stack.Screen

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen}/>
+        <Stack.Screen name="Home App" component={HomeScreen} options={{headerTitleAlign:'center'}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
