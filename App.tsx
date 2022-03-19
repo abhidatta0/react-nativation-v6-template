62e421975696cff670f74286718de55ddf00e18b
Abhirup <abhidatta146@gmail.com>
2022-03-19 21:03:44 +0530

    ♻️ moved Homescreen in a screen

--- App.tsx
+++ App.tsx
@@ -1,33 +1,7 @@
-import React, {useState} from 'react';
-import {
-  View,
-  Text,
-  Button,
-} from 'react-native';
-
-const HomeScreen = ()=>{
-  const [enthusiasmlevel, setEnthusiasmlevel] = useState(1);
-  
-  const increment = ()=> setEnthusiasmlevel(enthusiasmlevel +1);
-  const decrement = ()=> setEnthusiasmlevel(enthusiasmlevel -1);
-
-  const getExclamationMarks = (numChars: number)=> numChars > 0 ? Array(numChars+1).join('!') : '';
-
-  return (
-    <View>
-      <Text>Hello
-        {getExclamationMarks(enthusiasmlevel)}
-      </Text>
-      <View>
-        <Button title="Increase Enthusiasm" onPress={increment} color="blue" />
-        <Button title="Decrease Enthusiasm" onPress={decrement} color="red" />
-      </View>
-    </View>
-  )
-}
+import React from 'react';
+import HomeScreen from './src/screens/HomeScreen';
 
 const App = () => {
-  
   return (
     <HomeScreen />     
   );
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,25 @@
+import React, {useState} from 'react';
+import {View, Text, Button} from 'react-native';
+
+const HomeScreen = ()=>{
+    const [enthusiasmlevel, setEnthusiasmlevel] = useState(1);
+    
+    const increment = ()=> setEnthusiasmlevel(enthusiasmlevel +1);
+    const decrement = ()=> setEnthusiasmlevel(enthusiasmlevel -1);
+  
+    const getExclamationMarks = (numChars: number)=> numChars > 0 ? Array(numChars+1).join('!') : '';
+  
+    return (
+      <View>
+        <Text>Hello
+          {getExclamationMarks(enthusiasmlevel)}
+        </Text>
+        <View>
+          <Button title="Increase Enthusiasm" onPress={increment} color="blue" />
+          <Button title="Decrease Enthusiasm" onPress={decrement} color="red" />
+        </View>
+      </View>
+    )
+}
+
+export default HomeScreen;
